54938142070def250a8849badd18a9ee904642fb
saul <najerasaul@gmail.com>
2025-09-03 13:42:25 -0600

    Directorio inicial

---
+++ src/andpocalipse.lua
@@ -0,0 +1,18 @@
+function andpocalipse(cells)
+  local tab = {}
+  for i,v in pairs(cells) do
+    tab[v] = true
+  end
+  for i=1,23 do
+    if tab[i] then
+      tex.sprint("\\cellcolor[HTML]{C0C0C0}&")
+    else
+      tex.sprint("&")
+    end
+  end
+  if tab[24] then
+    tex.sprint("\\cellcolor[HTML]{C0C0C0}")
+  end
+end
+
+return { ANDpocalipse = andpocalipse }
